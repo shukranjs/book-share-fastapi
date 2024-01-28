22e29ebef5949ecfc31344d5a7e126c0a6b97beb
shukranjs <shukranrma@gmail.com>
2024-01-28 20:30:14 +0100

    add user & author

--- app/main.py
+++ app/main.py
@@ -1,21 +1,16 @@
 # app/main.py
-import os
 from fastapi import FastAPI
-from databases import Database
+from app.core.database import connect_to_database, close_database_connection
 
 app = FastAPI()
 
-# Use environment variables for database configuration
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://myuser:mypassword@postgres/mydatabase")
-database = Database(DATABASE_URL)
-
 @app.on_event("startup")
-async def startup_database():
-    await database.connect()
+async def startup_app():
+    await connect_to_database()
 
 @app.on_event("shutdown")
-async def shutdown_database():
-    await database.disconnect()
+async def shutdown_app():
+    await close_database_connection()
 
 @app.get("/")
 async def read_root():
